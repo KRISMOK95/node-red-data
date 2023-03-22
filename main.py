44a6bc81f78a0e273778a898a8dcdee27b35c20d
Ka Long Mok <kaxm462@gmail.com>
2023-03-22 23:14:14 +0100

    22-03-2023 status , alarm flags definition completed.

--- main.py
+++ main.py
@@ -33,9 +33,9 @@ def on_message(client, userdata, message):
     alarm_flag_3 = received_data[7]
     status_flag_2 = received_data[9]
     circulating_fluid_set_temperature = received_data[11]
-    ####### status flag ##########
     print(f" main: {received_data}")
 
+    ####### status flag ###########
 
     STATUS_FLAG_BIT_MEANING = {
         0: "Run flag",
@@ -58,28 +58,85 @@ def on_message(client, userdata, message):
         print(f"{meaning}: {bit_value}")
 
     time.sleep(2)
-    status_flag =received_data[5]
 
-    STATUS_FLAG_BIT_MEANING = {
-        0: "Run flag",
-        1: "Operation stop alarm flag",
-        2: "Operation continued alarm flag",
-        4: "Press Unit flag",
-        5: "Remote status flag",
-        9: "Completion of preparation (TEMP READY) flag",
-        10: "Temperature unit flag",
-        11: "Run timer flag",
-        12: "Stop timer flag",
-        13: "Reset after power failure flag",
-        14: "Anti-freezing flag",
-        15: "Automatic fluid filling flag",
+    ####### status flag 2 ###########
+
+    STATUS_FLAG_2_BIT_MEANING = {
+        0: "Electric resistivity/conductivity setting flag",
+        1: "Electric resistivity/conductivity setting flag",
     }
 
     # Extract the value of each bit and print the corresponding status
-    for bit, meaning in STATUS_FLAG_BIT_MEANING.items():
-        bit_value = (status_flag >> bit) & 1
+    for bit, meaning in STATUS_FLAG_2_BIT_MEANING.items():
+        bit_value = (status_flag_2 >> bit) & 1
         print(f"{meaning}: {bit_value}")
 
+    time.sleep(2)
+
+    ################### alarm flag 1 #################
+
+    ALARM_FLAG_1_BIT_MEANING = {
+        0: "Low level in tank",
+        1: "High circulating fluid discharge temp",
+        2: "Circulating fluid discharge temp. rise",
+        3: "Circulating fluid discharge temp.",
+        4: "High circulating fluid return temp..",
+        5: "High circulating fluid discharge pressure",
+        6: "Abnormal pump operation",
+        7: "Circulating fluid discharge pressure rise",
+        8: "Circulating fluid discharge pressure drop",
+        9: "High compressor intake temp",
+        10: "Low compressor intake temp.",
+        11: "Low super heat temperature",
+        12: "High compressor discharge pressure",
+        14: "Refrigerant circuit pressure (high pressure side) drop",
+        15: "Refrigerant circuit pressure (low pressure side) rise",
+    }
+    # Extract the value of each bit and print the corresponding status
+    for bit, meaning in ALARM_FLAG_1_BIT_MEANING.items():
+        bit_value = (alarm_flag_1 >> bit) & 1
+        print(f"Alarm flag: {meaning} /// {bit_value}")
+
+    time.sleep(2)
+    ################### alarm flag 2 #################
+
+    ALARM_FLAG_2_BIT_MEANING = {
+        0: "Refrigerant circuit pressure (low pressure side) drop",
+        1: "Compressor overload",
+        2: "Communication error",
+        3: "Memory error",
+        4: "DC line fuse cut",
+        5: "Circulating fluid discharge temp. sensor failure",
+        6: "Circulating fluid return temp. sensor failure",
+        7: "Compressor intake temp. sensor failure",
+        8: "Circulating fluid discharge pressure sensor failure",
+        9: "Compressor discharge pressure sensor failure",
+        10: "Compressor intake pressure sensor failure",
+        11: "Maintenance of pump",
+        12: "Maintenance of fan motor",
+        13: "Maintenance of compressor",
+        14: "Contact input 1 signal detection alarm",
+        15: "Contact input 2 signal detection alarm",
+    }
+    # Extract the value of each bit and print the corresponding status
+    for bit, meaning in ALARM_FLAG_2_BIT_MEANING.items():
+        bit_value = (alarm_flag_2 >> bit) & 1
+        print(f"Alarm flag: {meaning} /// {bit_value}")
+    time.sleep(2)
+    ################### alarm flag 2 #################
+
+    ALARM_FLAG_3_BIT_MEANING = {
+        0: "Water leakage",
+        1: "Electric resistivity/conductivity level rise",
+        2: "Electric resistivity/conductivity level drop",
+        3: "Electric resistivity/conductivity sensor error",
+
+    }
+    # Extract the value of each bit and print the corresponding status
+    for bit, meaning in ALARM_FLAG_3_BIT_MEANING.items():
+        bit_value = (alarm_flag_3 >> bit) & 1
+        print(f"Alarm flag: {meaning} /// {bit_value}")
+    time.sleep(2)
 
 # Set up the MQTT client and connect to the broker
 client = mqtt.Client()
